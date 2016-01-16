6ce09a4003cd56dbfe9058587afd7b71832cfbc5
Wang, Feng <feng01.wang@sap.com>
2016-01-17 02:27:38 +0800

    Q ajaxt promise chain

--- JavaScriptSample/promise/promiseWithQ.js
+++ JavaScriptSample/promise/promiseWithQ.js
@@ -15,6 +15,71 @@
         var message = "the result of first promise, will passed to the second promise.";
         return message;
     }).then(function(msg) {
-        writeln(msg)
+        writeln(msg);
     });
+
+    Q.delay(1000).thenResolve("thenResolve is a short cut of the then(function(){return something})").then(function(msg) {
+        writeln(msg);
+    });
+
+    var chainPromises = function() {
+        var getUser = function(id) {
+            return Q.delay(500).thenResolve({
+                "id": id,
+                "name": "User" + id
+            });
+        };
+        var getTweetsByUser = function(user) {
+            return Q.delay(600).thenResolve([
+                {
+                    "user": user.name,
+                    "message": "Tweet 1"
+                }, {
+                    "user": user.name,
+                    "message": "Tweet 2"
+                }
+            ]);
+        };
+        getUser(123).then(getTweetsByUser).then(function(tweets) {
+            tweets.forEach(function(t) {
+                writeln(t.user + " says: " + t.message);
+            });
+        });
+    };
+
+    chainPromises();
+
+    // Should disable Chrome web security to test: "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security
+    var qWithAjaxDemo = function() {
+
+        function wrapJQueryAjaxWithQ(ajaxSettings) {
+            var defer = Q.defer();
+            $.ajax(ajaxSettings).done(defer.resolve).fail(defer.reject);
+            return defer.promise;
+        }
+        var getFirstProduct = wrapJQueryAjaxWithQ({
+            "url": "http://services.odata.org/V4/OData/OData.svc/Products(0)",
+            "type": "GET"
+        });
+
+        function getNextProduct(previousProduct) {
+            writeln("Previous product: " + previousProduct.ID + " " + previousProduct.Name);
+            var id = previousProduct.ID + 1;
+            var call = wrapJQueryAjaxWithQ({
+                "url": "http://services.odata.org/V4/OData/OData.svc/Products(" + id + ")",
+                "type": "GET"
+            });
+            return call;
+        }
+
+        getFirstProduct.then(getNextProduct).then(getNextProduct).then(function(thirdProduct) {
+            writeln("Properties of the 3rd product:");
+            for ( var p in thirdProduct) {
+                writeln(thirdProduct[p]);
+            }
+        });
+    };
+
+    qWithAjaxDemo();
+
 })();
