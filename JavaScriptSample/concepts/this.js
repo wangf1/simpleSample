0bf1904deddc1541ef7b0d4901899b13dd70404b
Wang, Feng <feng01.wang@sap.com>
2016-01-21 16:18:15 +0800

    this key word

--- JavaScriptSample/concepts/this.js
+++ JavaScriptSample/concepts/this.js
@@ -8,7 +8,7 @@
      * "this" in function call refer to the global object.<br>
      * "this" in method call refer to the object itself.
      */
-    function globalScopeThis() {
+    function thisInFunctionCallAndMethodCall() {
         var result = this === window;
         writeln("this in a function call (rather than a method call) refer to the global object. 'this' is the window object? " + result);
 
@@ -16,8 +16,11 @@
             "property1": "value1",
             func: function() {
                 writeln("this in a method call refer the object. 'this' is the window object? " + (this === window));
+                var that = this;
                 function innerFunc() {
                     writeln("innerFunc() is also a normal function call, so this refer the global object. 'this' is the window object? " + (this === window));
+                    that.property1 = "the_new_value";
+                    writeln("inner function can use 'that' refer 'correct this i.e. the object', so we can set the 'property1' to a new value: " + that.property1);
                 }
                 innerFunc();
             }
@@ -25,9 +28,38 @@
 
         obj.func();
     }
+    /**
+     * "this" in constructor refer the newly created object.
+     */
+    function thisInConstructor() {
+        function Person(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        var john = new Person("John", 20);
+        writeln(john.name + " - " + john.age);
+    }
+
+    function thisInApplyAndCallMethod() {
+        function toApply(theOjbectToApplyAgainst) {
+            writeln("Apply this function to a object, the 'this' is the object to apply against: " + (this === theOjbectToApplyAgainst));
+            for (p in this) {
+                writeln("Property of 'this': " + p + ":" + this[p]);
+            }
+        }
+        var obj = {
+            "property1": "value1",
+            "property2": "value2"
+        };
+        toApply.apply(obj, obj);
+        toApply.call(obj, obj);
+    }
 
     $(document).ready(function() {
-        globalScopeThis();
+        thisInFunctionCallAndMethodCall();
+        thisInConstructor();
+        thisInApplyAndCallMethod();
     });
 
 })();
